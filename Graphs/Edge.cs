e69571071646a3bc4e0a0d5e41d64923db92cdb8
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-03-16 11:47:30 +0100

    Manuel added graph

---
+++ Graphs/Edge.cs
@@ -0,0 +1,14 @@
+﻿namespace VUPProjekt
+{
+    public class Edge<T>
+    {
+        public Node<T> From { get; private set; }
+        public Node<T> To { get; private set; }
+
+        public Edge(Node<T> from, Node<T> to)
+        {
+            this.To = to;
+            this.From = from;
+        }
+    }
+}
---
+++ Graphs/Graph.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace VUPProjekt
+{
+    public class Graph<T>
+    {
+
+        public List<Node<T>> NodeSet { get; private set; } = new List<Node<T>>();
+
+
+        public void AddNode(T value)
+        {
+            NodeSet.Add(new Node<T>(value));
+        }
+
+        public void AddDirectionEdge(T from, T to)
+        {
+            Node<T> fromNode = NodeSet.Find(x => x.Data.Equals(from));
+            Node<T> toNode = NodeSet.Find(x => x.Data.Equals(to));
+
+            if (!fromNode.Equals(default(T)) && !toNode.Equals(default(T)))
+            {
+                fromNode.AddEdge(toNode);
+            }
+            else
+            {
+               // Console.WriteLine("Node not found");
+            }
+
+        }
+
+        public void AddEdge(T from, T to)
+        {
+            Node<T> fromNode = NodeSet.Find(x => x.Data.Equals(from));
+
+            Node<T> toNode = NodeSet.Find(x => x.Data.Equals(to));
+
+            if (!fromNode.Equals(default(T)) && !toNode.Equals(default(T)))
+            {
+                fromNode.AddEdge(toNode);
+                toNode.AddEdge(fromNode);
+            }
+            else
+            {
+               // Console.WriteLine("Node not found!");
+            }
+
+
+        }
+
+    }
+}
