956ade56851bb1f9b8c196ee370723c0a7471d47
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-03-16 09:03:31 +0100

    Initial commit

---
+++ Interface.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GraphLibrary;
+
+namespace VUPProjekt
+{
+    
+
+    public class Interface
+    {
+        public List<string> Cities = new List<string>();
+
+        public event EventHandler ButtonClick;
+
+
+        private void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        private void LoadContent(ContentManager content)
+        {
+
+        }
+
+        protected virtual void OnButtonClick()
+        {
+            ButtonClick?.Invoke(this, new EventArgs());
+        }
+
+
+        private static List<Node<T>> TrackPath<T>(Node<T> node, Node<T> start)
+        {
+            List<Node<T>> path = new List<Node<T>>();
+
+            while (!node.Equals(start))
+            {
+                path.Add(node);
+                node = node.Parent;
+            }
+            path.Add(start);
+
+            path.Reverse();
+
+
+            return path;
+        }
+
+
+        private static Node<T> DFS<T>(Node<T> start, Node<T> goal)
+        {
+            Stack<Edge<T>> edges = new Stack<Edge<T>>();
+            edges.Push(new Edge<T>(start, start));
+
+            while (edges.Count > 0)
+            {
+                Edge<T> edge = edges.Pop();
+
+                if (!edge.To.Discovered)
+                {
+                    edge.To.Discovered = true;
+                    edge.To.Parent = edge.From;
+
+                }
+                if (edge.To == goal)
+                {
+                    return edge.To;
+                }
+
+                foreach (Edge<T> e in edge.To.Edges)
+                {
+                    if (!e.To.Discovered)
+                    {
+                        edges.Push(e);
+                    }
+                }
+
+            }
+
+            return null;
+        }
+
+
+
+        private static Node<T> BFS<T>(Node<T> start, Node<T> goal)
+        {
+            Queue<Edge<T>> edges = new Queue<Edge<T>>();
+            edges.Enqueue(new Edge<T>(start, start));
+
+            while (edges.Count > 0)
+            {
+                Edge<T> edge = edges.Dequeue();
+
+                if (!edge.To.Discovered)
+                {
+                    edge.To.Discovered = true;
+                    edge.To.Parent = edge.From;
+                }
+                if (edge.To == goal)
+                {
+                    return edge.To;
+                }
+
+                foreach (Edge<T> e in edge.To.Edges)
+                {
+                    if (!e.To.Discovered)
+                    {
+                        edges.Enqueue(e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
+    }
+}
---
+++ Road.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VUPProjekt
+{
+    public class Road
+    {
+        
+        public List<Road> Roads = new List<Road>();
+
+        private Vector2 position;
+        private bool isVisible;
+
+        public bool IsVisible { get => isVisible; set => isVisible = value; }
+        public Vector2 Position { get => position; set => position = value; }
+
+
+        private void DisplayRoad()
+        {
+
+        }
+
+        private void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        private void LoadContent(ContentManager content)
+        {
+
+        }
+
+    }
+}
